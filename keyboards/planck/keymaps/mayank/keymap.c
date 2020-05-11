39deffeb550e5ac0fe6c71b4aae76db84f51b512
Mayank Sharma <sharmamm@mail.uc.edu>
2020-05-11 18:44:29 -0400

    Updated Planck keymap

--- keyboards/planck/keymaps/mayank/keymap.c
+++ keyboards/planck/keymaps/mayank/keymap.c
@@ -2,23 +2,26 @@
 
 #define _______ KC_TRNS
 
-#define LW_TAB  LGUI_T(KC_TAB)  // Super key if held, Tab if tapped
+#define LW_TAB  LGUI_T(KC_TAB)      // Super key if held, Tab if tapped
 
-#define SUPER_1 LGUI(KC_1)      // Super/CMD + 1
-#define SUPER_2 LGUI(KC_2)      // Super/CMD + 2
-#define SUPER_3 LGUI(KC_3)      // Super/CMD + 3
-#define SUPER_4 LGUI(KC_4)      // Super/CMD + 4
-#define SUPER_5 LGUI(KC_5)      // Super/CMD + 5
-#define SUPER_6 LGUI(KC_6)      // Super/CMD + 6
-#define SUPER_7 LGUI(KC_7)      // Super/CMD + 7
-#define SUPER_8 LGUI(KC_8)      // Super/CMD + 8
-#define SUPER_9 LGUI(KC_9)      // Super/CMD + 9
+#define SUPER_1 LGUI(KC_1)          // Super/CMD + 1
+#define SUPER_2 LGUI(KC_2)          // Super/CMD + 2
+#define SUPER_3 LGUI(KC_3)          // Super/CMD + 3
+#define SUPER_4 LGUI(KC_4)          // Super/CMD + 4
+#define SUPER_5 LGUI(KC_5)          // Super/CMD + 5
+#define SUPER_6 LGUI(KC_6)          // Super/CMD + 6
+#define SUPER_7 LGUI(KC_7)          // Super/CMD + 7
+#define SUPER_8 LGUI(KC_8)          // Super/CMD + 8
+#define SUPER_9 LGUI(KC_9)          // Super/CMD + 9
 
-#define W_PREV  LCTL(KC_LEFT)   // Move to previous workspace on Mac OS
-#define W_NEXT  LCTL(KC_RGHT)   // Move to next workspace on Mac OS
-#define M_CTRL  LALT(KC_UP)     // Open Mission Control on Mac OS
+#define W_PREV  LCTL(KC_LEFT)       // Move to previous workspace on Mac OS
+#define W_NEXT  LCTL(KC_RGHT)       // Move to next workspace on Mac OS
+#define M_CTRL  LALT(KC_UP)         // Open Mission Control on Mac OS
+#define HLIGHT  LCTL(LGUI(KC_H))    // Highlight text in Preview on Mac OS
+#define SSHOT1  SGUI(KC_3)          // Screenshot entire screen on Mac OS
+#define SSHOT2  SGUI(KC_4)          // Screenshot selected area on Mac OS
 
-#define OSLAYER LT(4, KC_SPC)   // Activate layer 4 when held, space when tapped
+#define OSLAYER LT(4, KC_SPC)       // Activate layer 4 when held, space when tapped
 
 /* Macros for email IDs */
 enum custom_keycodes {
@@ -51,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------.
      * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |UCMAIL|  &   |  *   |  (   |  )   |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * | LGUI |      | Prev | Play | Next |GMAIL1|      |      |  $   |  %   |  ^   |  _   |
+     * | LGUI |      | Prev | Play | Next |GMAIL1|HLIGHT|      |  $   |  %   |  ^   |  _   |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |      | Vol- | Mute | Vol+ |      |      |      |  !   |  @   |  #   |  +   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -60,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [1] = LAYOUT_ortho_4x12(
         KC_F1,   KC_F2  , KC_F3  , KC_F4,   KC_F5,   KC_F6,   _______, UCMAIL,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-        KC_LGUI, _______, KC_MPRV, KC_MPLY, KC_MNXT, GMAIL1,  _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS,
+        KC_LGUI, _______, KC_MPRV, KC_MPLY, KC_MNXT, GMAIL1,  HLIGHT,  _______, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS,
         _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_PLUS,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
@@ -69,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------.
      * |  `   |      |      |      |      |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |      |      |      |      |GMAIL2| Left | Down |  Up  |Right |      |      |
+     * |      |      |SShot1|      |      |GMAIL2| Left | Down |  Up  |Right |      |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |      |W_Prev|M_Ctrl|W_Next|      |      |      |      |      |  \   |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -78,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [2] = LAYOUT_ortho_4x12(
         KC_GRV,  _______, _______, _______, _______, _______, KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, _______, _______, _______, _______, GMAIL2,  KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______, _______,
+        _______, _______, SSHOT1,  _______, _______, GMAIL2,  KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______, _______,
         _______, _______, W_PREV,  M_CTRL,  W_NEXT,  _______, _______, _______,  _______, _______, KC_BSLS, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______
     ),
@@ -105,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------.
      * |      |      |      |      |      |      |      |      | OS+7 | OS+8 | OS+9 |      |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      | OS+4 | OS+5 | OS+6 |      |
+     * |      |      |SShot2|      |      |      |      |      | OS+4 | OS+5 | OS+6 |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |      |      |      |      |      |      |      | OS+1 | OS+2 | OS+3 |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -114,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [4] = LAYOUT_ortho_4x12(
         _______, _______, _______, _______, _______, _______, _______, _______, SUPER_7, SUPER_8, SUPER_9, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, SUPER_4, SUPER_5, SUPER_6, _______,
+        _______, _______, SSHOT2,  _______, _______, _______, _______, _______, SUPER_4, SUPER_5, SUPER_6, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, SUPER_1, SUPER_2, SUPER_3, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ) 
