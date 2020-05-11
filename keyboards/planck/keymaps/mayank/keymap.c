e77919f11737c05b008e4680cdd0127c3b3e06fe
Mayank Sharma <sharmamm@mail.uc.edu>
2020-05-10 22:48:52 -0400

    Updated Planck keymap to be the same as Niu Mini

--- keyboards/planck/keymaps/mayank/keymap.c
+++ keyboards/planck/keymaps/mayank/keymap.c
@@ -1,5 +1,3 @@
-/* Definitely not the most efficient use of layers but 
-   has most of the functionality needed */
 #include QMK_KEYBOARD_H
 
 #define _______ KC_TRNS
@@ -20,10 +18,10 @@
 #define W_NEXT  LCTL(KC_RGHT)   // Move to next workspace on Mac OS
 #define M_CTRL  LALT(KC_UP)     // Open Mission Control on Mac OS
 
-#define LT_NUM  LT(2, KC_SPC)
+#define OSLAYER LT(4, KC_SPC)   // Activate layer 4 when held, space when tapped
 
 /* Macros for email IDs */
-enum custom_keycodes{
+enum custom_keycodes {
   UCMAIL = SAFE_RANGE,
   GMAIL1,
   GMAIL2
@@ -39,53 +37,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Ctrl | PgUp | PgDn | Alt  |Layer1|Space |Space |Layer2|Layer3|   [  |   ]  |  "   |
+     * | Ctrl | PgUp | PgDn | Alt  |Layer1|4/Spc |Space |Layer2|Layer3|   [  |   ]  |  ""  |
      * `-----------------------------------------------------------------------------------'
      */
-    [0] = LAYOUT_ortho_4x12 (
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-        LW_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_PGUP, KC_PGDN, KC_LALT, MO(1), LT_NUM, KC_SPC, MO(2), MO(3),   KC_LBRC, KC_RBRC, KC_QUOT
+    [0] = LAYOUT_ortho_4x12(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+        LW_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_PGUP, KC_PGDN, KC_LALT, MO(1), OSLAYER, KC_SPC, MO(2), MO(3),   KC_LBRC, KC_RBRC, KC_QUOT
     ),
 
     /* Layer 1
      * ,-----------------------------------------------------------------------------------.
-     * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |      |LGUI+7|LGUI+8|LGUI+9|      |
+     * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |UCMAIL|  &   |  *   |  (   |  )   |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * | LGUI |      | Prev | Play | Next |GMAIL1|      |      |LGUI+4|LGUI+5|LGUI+6|      |
+     * | LGUI |      | Prev | Play | Next |GMAIL1|      |      |  $   |  %   |  ^   |  _   |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      | Vol- | Mute | Vol+ |      |      |      |LGUI+1|LGUI+2|LGUI+3|      |
+     * |      |      | Vol- | Mute | Vol+ |      |      |      |  !   |  @   |  #   |  +   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * |      |      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [1] = LAYOUT_ortho_4x12 (
-        KC_F1,   KC_F2  , KC_F3  , KC_F4,   KC_F5,   KC_F6,   _______, UCMAIL,  SUPER_7, SUPER_8, SUPER_9, _______,
-        KC_LGUI, _______, KC_MPRV, KC_MPLY, KC_MNXT, GMAIL1,  _______, _______, SUPER_4, SUPER_5, SUPER_6, _______,
-        _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, SUPER_1, SUPER_2, SUPER_3, _______,
+    [1] = LAYOUT_ortho_4x12(
+        KC_F1,   KC_F2  , KC_F3  , KC_F4,   KC_F5,   KC_F6,   _______, UCMAIL,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+        KC_LGUI, _______, KC_MPRV, KC_MPLY, KC_MNXT, GMAIL1,  _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS,
+        _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_PLUS,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
-    
-    /* Layer 2 
-     * ,-----------------------------------------------------------------------------------.
-     * |      |      |      |      |      |      |      |      |  &   |  *   |  (   |  )   |
-     * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |  $   |  %   |  ^   |  _   |
-     * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |  !   |  @   |  #   |  +   |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |      |      |      |      |
-     * `-----------------------------------------------------------------------------------'
-     */
-    [2] = LAYOUT_ortho_4x12 (
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_PLUS,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ), 
 
-    /* Layer 3 
+    /* Layer 2 
      * ,-----------------------------------------------------------------------------------.
      * |  `   |      |      |      |      |      |  F7  |  F8  |  F9  | F10  | F11  | F12  |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -96,29 +76,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [3] = LAYOUT_ortho_4x12 (
+    [2] = LAYOUT_ortho_4x12(
         KC_GRV,  _______, _______, _______, _______, _______, KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,
         _______, _______, _______, _______, _______, GMAIL2,  KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______, _______,
         _______, _______, W_PREV,  M_CTRL,  W_NEXT,  _______, _______, _______,  _______, _______, KC_BSLS, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______
     ),
     
-    /* Layer 4 
+    /* Layer 3 
      * ,-----------------------------------------------------------------------------------.
-     * |      |      |      |      |      |      |      |      |  7   |  8   |  9   |  0   |
+     * |  ~   | RGB  |Mode- |Mode+ | Hue- | Hue+ |      |      |  7   |  8   |  9   |  0   |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |  4   |  5   |  6   |  -   |
+     * |      | Sat- | Sat+ |Brite-|Brite+|Plain |      |      |  4   |  5   |  6   |  -   |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |  1   |  2   |  3   |  =   |
+     * |      |Breath|Rainbo|Swirl |Snake |Grad  |      |      |  1   |  2   |  3   |  =   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |      |      |      |Reset |
+     * |      |      |      |      |      |      |      |      |      |Speed-|Speed+|Reset |
      * `-----------------------------------------------------------------------------------'
      */
-    [4] = LAYOUT_ortho_4x12 (
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_0,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    KC_MINS,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_EQL,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET
+    [3] = LAYOUT_ortho_4x12(
+        KC_TILD, RGB_TOG, RGB_RMOD, RGB_MOD,  RGB_HUD,  RGB_HUI,  _______, _______,  KC_7,    KC_8,    KC_9,    KC_0,
+        _______, RGB_SAD, RGB_SAI,  RGB_VAD,  RGB_VAI,  RGB_M_P,  _______, _______,  KC_4,    KC_5,    KC_6,    KC_MINS,
+        _______, RGB_M_B, RGB_M_R,  RGB_M_SW, RGB_M_SN, RGB_M_G,  _______, _______,  KC_1,    KC_2,    KC_3,    KC_EQL,
+        _______, _______, _______,  _______, _______,  _______,   _______, _______,  _______, RGB_SPD, RGB_SPI, RESET
+    ), 
+    
+    /* Layer 4 
+     * ,-----------------------------------------------------------------------------------.
+     * |      |      |      |      |      |      |      |      | OS+7 | OS+8 | OS+9 |      |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      | OS+4 | OS+5 | OS+6 |      |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      | OS+1 | OS+2 | OS+3 |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [4] = LAYOUT_ortho_4x12(
+        _______, _______, _______, _______, _______, _______, _______, _______, SUPER_7, SUPER_8, SUPER_9, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, SUPER_4, SUPER_5, SUPER_6, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, SUPER_1, SUPER_2, SUPER_3, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ) 
 };
 
@@ -131,30 +129,30 @@ void matrix_scan_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case UCMAIL:
-          if (record->event.pressed) {
-            // when UCMAIL is pressed
-            SEND_STRING ("sharmamm@mail.uc.edu");
-          } else {
-            // when UCMAIL is released
-          }
-          break;
-        case GMAIL1:
-          if (record->event.pressed) {
-            // when GMAIL1 is pressed
-            SEND_STRING ("mayanksharma1806@gmail.com");
-          } else {
-            // when GMAIL1 is released
-          }
-          break;
-        case GMAIL2:
-          if (record->event.pressed) {
-            // when GMAIL2 is pressed
-            SEND_STRING ("onlyforotherapps2016@gmail.com");
-          } else {
-            // when GMAIL2 is released
-          }
-          break;
+      case UCMAIL:
+        if (record->event.pressed) {
+          // when UCMAIL is pressed
+          SEND_STRING ("sharmamm@mail.uc.edu");
+        } else {
+          // when UCMAIL is released
+        }
+        break;
+      case GMAIL1:
+        if (record->event.pressed) {
+          // when GMAIL1 is pressed
+          SEND_STRING ("mayanksharma1806@gmail.com");
+        } else {
+          // when GMAIL1 is released
+        }
+        break;
+      case GMAIL2:
+        if (record->event.pressed) {
+          // when GMAIL2 is pressed
+          SEND_STRING ("onlyforotherapps2016@gmail.com");
+        } else {
+          // when GMAIL2 is released
+        }
+        break;
     }
     return true;
 }
