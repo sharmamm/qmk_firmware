1ab374024a12ad5dcbd8a9d67a89bb41af6b5790
Mayank Sharma <sharmamm@mail.uc.edu>
2020-07-12 18:16:47 -0400

    Modified Enter to be Shift when held on Niu Mini

--- keyboards/niu_mini/keymaps/mayank/keymap.c
+++ keyboards/niu_mini/keymaps/mayank/keymap.c
@@ -3,6 +3,7 @@
 #define _______ KC_TRNS
 
 #define LW_TAB  LGUI_T(KC_TAB)      // Super key if held, Tab if tapped
+#define LW_ENT  LSFT_T(KC_ENT)      // Left shift if held, Enter if tapped
 
 #define SUPER_1 LGUI(KC_1)          // Super/CMD + 1
 #define SUPER_2 LGUI(KC_2)          // Super/CMD + 2
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x12(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
         LW_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, LW_ENT,
         KC_LCTL, KC_PGUP, KC_PGDN, KC_LALT, MO(1), OSLAYER, KC_SPC, MO(2), MO(3),   KC_LBRC, KC_RBRC, KC_RSFT
     ),
 
